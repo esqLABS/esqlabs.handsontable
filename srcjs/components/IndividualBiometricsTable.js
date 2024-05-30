2431f9fdf1be945571de1fd146ef495df0c08e29
AKostiv8 <nolik2000@yahoo.com>
2024-05-30 14:30:59 +0300

    update population value cell based on species

--- srcjs/components/IndividualBiometricsTable.js
+++ srcjs/components/IndividualBiometricsTable.js
@@ -1,27 +1,73 @@
-import React, { useState } from "react";
-import { useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { HotTable, HotColumn } from "@handsontable/react";
 import { registerAllModules } from "handsontable/registry";
+import { dropdownRenderer } from "handsontable/renderers";
 import "handsontable/dist/handsontable.full.min.css";
+// Import Custom Renderer
+import { readOnlyStyleRenderer } from "./TableRenderer/TableRenderer";
 
 function IndividualBiometricsTable(props) {
   // Data state
   const [dataR, updateDataR] = useState(props.data_scenarios);
   const col_names = Object.keys(dataR[0]);
+  const hotTableComponentRef = useRef(null);
+
+  useEffect(() => {
+    const hot = hotTableComponentRef.current.hotInstance;
+
+    hot.updateSettings({
+      cells(row, col) {
+        const cellProperties = {};
+
+        // Check if the column is "population"
+        if (col === col_names.indexOf(col_names[1])) {
+          if (
+            hot.getData()[row][col - 1].toLowerCase() === "Human".toLowerCase()
+          ) {
+            cellProperties.readOnly = true;
+            cellProperties.type = "text";
+            cellProperties.renderer = readOnlyStyleRenderer;
+          } else {
+            cellProperties.readOnly = false;
+            cellProperties.type = "dropdown";
+            cellProperties.source = props.population_options;
+            cellProperties.renderer = dropdownRenderer;
+          }
+        }
+
+        return cellProperties;
+      },
+    });
+  });
+
+  const updateNeighbourReadOnly = (changes, dataR) => {
+    // changes: [[<row_number>, <column_name>, <previous_value>, <new_value>]]
+    if (
+      changes[0][1] === col_names[1] &&
+      changes[0][3].toLowerCase() === "Human".toLowerCase()
+    ) {
+      dataR[changes[0][0]][col_names[2]] = null;
+    }
+  };
 
   const onBeforeHotChange = (changes) => {
     if (changes === undefined) return;
     if (changes === null) return;
     if (!changes.length) return;
     if (dataR[changes[0][0]][changes[0][1]] === changes[0][3]) {
-        // console.log("no change");
-        return;
+      // console.log("no change");
+      return;
     } else {
-        setTimeout(() => {
-            // console.log(prepareShinyData(dataR));
-            // Send data to Shiny with the edited data
-            Shiny.setInputValue(`${props.shiny_el_id_name}_edited`, JSON.stringify(dataR), {priority: "event"});
-        }, 500)
+      updateNeighbourReadOnly(changes, dataR);
+      setTimeout(() => {
+        // console.log(prepareShinyData(dataR));
+        // Send data to Shiny with the edited data
+        Shiny.setInputValue(
+          `${props.shiny_el_id_name}_edited`,
+          JSON.stringify(dataR),
+          { priority: "event" }
+        );
+      }, 500);
     }
   };
 
@@ -37,32 +83,58 @@ function IndividualBiometricsTable(props) {
       licenseKey="non-commercial-and-evaluation"
       contextMenu={{
         items: {
-          'cut': {
-            name: 'Clear'
+          cut: {
+            name: "Clear",
           },
-          'row_below': {},
-          'remove_row': {
+          row_below: {},
+          remove_row: {
             disabled() {
-            // Disable option when first row was clicked
-            return this.getSelectedLast()[0] === 0; // `this` === hot
-            }
-          }
-        }
+              // Disable option when first row was clicked
+              return this.getSelectedLast()[0] === 0; // `this` === hot
+            },
+          },
+        },
       }}
       beforeChange={onBeforeHotChange}
       afterRemoveRow={(index, amount, physicalRows) => {
         // Send data to Shiny with the edited data
-        Shiny.setInputValue(`${props.shiny_el_id_name}_edited`, JSON.stringify(dataR), {priority: "event"});
+        Shiny.setInputValue(
+          `${props.shiny_el_id_name}_edited`,
+          JSON.stringify(dataR),
+          { priority: "event" }
+        );
       }}
       afterCreateRow={(index, amount) => {
         // Send data to Shiny with the edited data
-        Shiny.setInputValue(`${props.shiny_el_id_name}_edited`, JSON.stringify(dataR), {priority: "event"});
+        Shiny.setInputValue(
+          `${props.shiny_el_id_name}_edited`,
+          JSON.stringify(dataR),
+          { priority: "event" }
+        );
       }}
     >
       <HotColumn settings={{ data: col_names[0], type: "text" }} />
-      <HotColumn settings={{ data: col_names[1], type: "dropdown", source: props.species_options }} />
-      <HotColumn settings={{ data: col_names[2], type: "dropdown", source: props.population_options }} />
-      <HotColumn settings={{ data: col_names[3], type: "dropdown", source: props.gender_options }} />
+      <HotColumn
+        settings={{
+          data: col_names[1],
+          type: "dropdown",
+          source: props.species_options,
+        }}
+      />
+      <HotColumn
+        settings={{
+          data: col_names[2],
+          type: "dropdown",
+          source: props.population_options,
+        }}
+      />
+      <HotColumn
+        settings={{
+          data: col_names[3],
+          type: "dropdown",
+          source: props.gender_options,
+        }}
+      />
       <HotColumn settings={{ data: col_names[4], type: "numeric" }} />
       <HotColumn settings={{ data: col_names[5], type: "numeric" }} />
       <HotColumn settings={{ data: col_names[6], type: "numeric" }} />
---
+++ srcjs/components/TableRenderer/TableRenderer.js
@@ -0,0 +1,14 @@
+import { textRenderer } from 'handsontable/renderers';
+
+export function readOnlyStyleRenderer(
+  instance,
+  td,
+  row,
+  col,
+  prop,
+  value,
+  cellProperties
+) {
+  textRenderer.apply(this, arguments);
+  td.style.background = "#eeeeee";
+}
